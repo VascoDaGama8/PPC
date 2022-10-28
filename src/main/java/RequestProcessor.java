1a5e14e4f798f8828e8a0b577c580a3704cbed1f
GladyshevSergey <serg.gladysh3v@yandex.ru>
2022-10-29 03:41:07 +0700

    Model write/read

--- src/main/java/RequestProcessor.java
+++ src/main/java/RequestProcessor.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.ArrayList;
 import java.util.Random;
 
 class RequestProcessor extends Thread //for multi-threaded server
@@ -13,57 +15,41 @@ class RequestProcessor extends Thread //for multi-threaded server
     public void run()
     {
         try
-        {//Declaring properties and streams
-            OutputStream outputStream=socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter=new OutputStreamWriter(outputStream);
-            InputStream inputStream=socket.getInputStream();
-            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
-            StringBuffer stringBuffer;
-            String response;
-            String request;
-            char x;
-            int y = 0;
-            String name;
-            String gender;//getting input stream and its reader, for reading request or acknowledgement
-            stringBuffer=new StringBuffer();
-            while(true)
-            {
-                Thread.sleep(3000);
-                Random r = new Random();
-                x  = (char) r.nextInt(100);
-                System.out.println(x);
-                outputStreamWriter.write(x);
-//                y++;
-                if(y == 0) {
-                    outputStreamWriter.write(-1);
-                    break;
-                }
-            }
-            System.out.println("End thread");
-//            request=stringBuffer.toString();
-//            System.out.println("Request : "+request);//parsing and extracting Request data
-//            temp1=request.indexOf(",");
-//            temp2=request.indexOf(",",temp1+1);
-//            part1=request.substring(0,temp1);
-//            part2=request.substring(temp1+1,temp2);
-//            part3=request.substring(temp2+1);
-//            rollNumber=Integer.parseInt(part1);
-//            name=part2;
-//            gender=part3;
-//            System.out.println("Roll number : "+rollNumber);
-//            System.out.println("Name : "+name);
-//            System.out.println("Gender : "+gender);
-//    // handle data//sending response
-//            response="Data saved#";//get output stream and its writer, for sending response or acknowledgement
-//            outputStream=socket.getOutputStream();
-//            outputStreamWriter=new OutputStreamWriter(outputStream);
-//            outputStreamWriter.write(response);
-            outputStreamWriter.flush(); // response sent
-//            System.out.println("Response sent");
-            socket.close(); //terminating connection
-        }catch(Exception exception)
         {
-            System.out.println(exception);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String x = "We will send you equation\n";
+            String y;
+            ArrayList <String> buf = new ArrayList<>();
+            out.write(x);
+            long time = System.currentTimeMillis();
+            while (true) {
+                System.out.println("n");
+                out.write("1\n");
+                out.flush();
+                time = System.nanoTime();
+
+                Thread.sleep(3000);
+                if((y = in.readLine()) != null){
+                    buf.add(y);
+                    System.out.println(y);
+                }
+                System.out.println("k");
+            }
+//            String inputLine;
+//            while ((inputLine = in.readLine()) != null) {
+//                if (".".equals(inputLine)) {
+//                    out.println("bye");
+//                    break;
+//                }
+//                out.println(inputLine);
+//            }
+//            in.close();
+//            out.flush();
+//            socket.close();
+        } catch (IOException | InterruptedException ioException) {
+            ioException.printStackTrace();
         }
     }
 }
